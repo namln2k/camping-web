2c66380e50e7b5f947578ee2a7d41b5652fe45d8
namln2k <namln2aug2k@gmail.com>
2024-01-29 19:04:37 +0700

    Temp: Image placeholder

--- next.config.mjs
+++ next.config.mjs
@@ -5,21 +5,25 @@ const nextConfig = {
     MAGENTO_BASE_URL: process.env.MAGENTO_BASE_URL,
     MAGENTO_BASE_MEDIA_URL: `${process.env.MAGENTO_BASE_URL}/pub/media`,
     MAGENTO_BASE_MEDIA_CATALOG_PRODUCT_URL: `${process.env.MAGENTO_BASE_URL}/media/catalog/product`,
-    BASE_URL: process.env.BASE_URL
+    BASE_URL: process.env.BASE_URL,
   },
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: new URL(process.env.MAGENTO_BASE_URL).hostname
+        protocol: 'http',
+        hostname: new URL(process.env.MAGENTO_BASE_URL).hostname,
       },
       {
-        protocol: "https",
-        hostname: new URL(process.env.MAGENTO_BASE_URL).hostname
-      }
-    ]
-  }
+        protocol: 'https',
+        hostname: new URL(process.env.MAGENTO_BASE_URL).hostname,
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 }
 
-module.exports = nextConfig
+export default nextConfig
--- src/components/catalog/product/ProductCard.tsx
+++ src/components/catalog/product/ProductCard.tsx
@@ -10,7 +10,7 @@ interface Props {
   className?: string
 }
 
-export default function ProductCard({ product, className = '' }: Props) {
+export default async function ProductCard({ product, className = '' }: Props) {
   const router = useRouter()
 
   return (
@@ -26,27 +26,27 @@ export default function ProductCard({ product, className = '' }: Props) {
         width={280}
         height={400}
       />
-      <div className="p-4">
-        <p className="pb-4 font-semibold text-xl">{product.name}</p>
-        <div className="my-2">
+      <div className='p-4'>
+        <p className='pb-4 font-semibold text-xl'>{product.name}</p>
+        <div className='my-2'>
           <ProductRating
             ratingSummary={product.rating_summary}
             reviewCount={product.review_count}
           />
         </div>
-        <div className="mt-6">
-          <IntlProvider locale="en">
+        <div className='mt-6'>
+          <IntlProvider locale='en'>
             <ProductPartialPrice
-              className="inline-flex"
+              className='inline-flex'
               price={{ ...product.price_range.minimum_price }}
             />
           </IntlProvider>
         </div>
       </div>
       <button
-        type="button"
-        aria-label="Shop now"
-        className="text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:bg-gradient-to-br shadow-lg shadow-blue-500/50 rounded-lg font-bold py-3 text-center focus:ring-0 capitalize"
+        type='button'
+        aria-label='Shop now'
+        className='text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:bg-gradient-to-br shadow-lg shadow-blue-500/50 rounded-lg font-bold py-3 text-center focus:ring-0 capitalize'
       >
         Shop Now
       </button>
