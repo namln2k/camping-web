fb7efb2da2eb3eb366ec9216210795dfaadc3a5a
namln2k <namln2aug2k@gmail.com>
2024-01-30 18:15:02 +0700

    Temp: Select options for product

--- src/components/catalog/product/detail/ConfigurableProduct.tsx
+++ src/components/catalog/product/detail/ConfigurableProduct.tsx
@@ -2,56 +2,41 @@ import ProductGallery from "@/components/catalog/product/ProductGallery";
 import ProductPartialPrice from "@/components/catalog/product/ProductPartialPrice";
 import ProductRating from "@/components/catalog/product/ProductRating";
 import Swatch from "@/components/catalog/product/detail/Swatch";
-import { ConfigurableOption, ConfigurableOptionValue, ProductDetail, ProductVariant } from "@/types";
+import useProductDetail from "@/hooks/catalog/useProductDetail";
+import useQuery from "@/hooks/useQuery";
+import { ProductDetail, ProductVariant } from "@/types";
+import { useCallback, useEffect, useState } from "react";
 
 interface Props {
     product: ProductDetail
     className?: string
 }
 
-const renderConfigurableOptions = (configurableOptions: ConfigurableOption[], variants: ProductVariant[]) => {
-    console.log(configurableOptions);
-    
-    const handleOptionSelect = ({ uid, value_index }: ConfigurableOptionValue) => {
-        // Get product variant by selected option's uid and value_index
-        // const variant = variants.find((variant) => {
-        //     return variant.product.sku === uid && variant.product.media_gallery_entries[0].position === value_index
-        // })
-        console.log(variants[0]);
-        
-    }
+interface SelectedOption {
+    attributeCode: string
+    valueUid: string
+}
 
-    return configurableOptions.map((option) => {
-        const { attribute_code, label, values } = option
-        return (
-            <div key={attribute_code} className="mb-4">
-                <h3 className="mb-2 text-sm font-semibold text-gray-600 uppercase">
-                    {label}
-                </h3>
-                <div className="flex flex-wrap">
-                    {values.map((value) => {
-                        const { uid, label, swatch_data } = value
+const findVariantBySelectedOptions = (variants: ProductVariant[], selectedOptions: SelectedOption[]): ProductVariant | null => {
+    let result = null
 
-                        return (
-                            <div key={uid} className="flex flex-wrap gap-2 p-2" onClick={() => handleOptionSelect(value)}>
-                                {swatch_data ? (
-                                    <div className="w-12 h-12 hover:shadow-[0_0_6px_0_rgb(71,139,255)] rounded">
-                                        <Swatch {...swatch_data} key={uid} />
-                                    </div>
-                                ) : (
-                                    <div
-                                        key={uid}
-                                        className="flex items-center justify-center w-12 h-12 mr-4 mb-4 text-sm text-center text-gray-700 bg-gray-100 border rounded-full cursor-pointer hover:shadow-[0_0_4px_0_rgb(71,139,255)]">
-                                        {label}
-                                    </div>
-                                )}
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        )
+    variants.forEach((variant) => {
+        const { attributes } = variant
+
+        const matched = attributes.every((attribute) => {
+            const { code, uid } = attribute
+            const selectedOption = selectedOptions.find((option) => {
+                return option.attributeCode === code && option.valueUid === uid
+            })
+            return selectedOption
+        })
+
+        if (matched) {
+            result = variant
+        }
     })
+
+    return result
 }
 
 export default function ConfigurableProduct({ product, className = '' }: Props) {
@@ -65,6 +50,93 @@ export default function ConfigurableProduct({ product, className = '' }: Props) 
         configurable_options,
         media_gallery_entries
     } = product
+
+    const [selectedOptions, setSelectedOptions] = useState<SelectedOption[]>([])
+    const [selectedProductSku, setSelectedProductSku] = useState<string>()
+    const [mediaGalleryEntries, setMediaGalleryEntries] = useState(media_gallery_entries)
+
+    const findSelectedOptionIndex = (option: SelectedOption, selectedOptions: SelectedOption[]) => {
+        return selectedOptions.findIndex((selectedOption) => {
+            return selectedOption.attributeCode === option.attributeCode
+        })
+    }
+
+    const isSelected = useCallback((option: SelectedOption) => {
+        return selectedOptions.findIndex((selectedOption) => {
+            return selectedOption.attributeCode === option.attributeCode && selectedOption.valueUid === option.valueUid
+        })
+    }, [selectedOptions])
+
+    const handleOptionSelect = useCallback((selectedOption: SelectedOption) => {
+        setSelectedOptions(() => {
+            // Find selected option with exact attribute code. If exists, replace it with new selected option. Otherwise, add new selected option.
+            const selectedOptionIndex = findSelectedOptionIndex(selectedOption, selectedOptions)
+
+            if (selectedOptionIndex > -1) {
+                return [
+                    ...selectedOptions.slice(0, selectedOptionIndex),
+                    selectedOption,
+                    ...selectedOptions.slice(selectedOptionIndex + 1)
+                ]
+            } else {
+                return [...selectedOptions, selectedOption]
+            }
+        })
+    }, [setSelectedOptions, selectedOptions])
+
+    useEffect(() => {
+        // TODO: Get child product's media gallery entries
+
+    }, [selectedProductSku, setMediaGalleryEntries])
+    
+
+    const renderConfigurableOptions = useCallback(() => {
+        return configurable_options.map((option) => {
+            const { attribute_code, label, values } = option
+            return (
+                <div key={attribute_code} className="mb-4">
+                    <h3 className="mb-2 text-sm font-semibold text-gray-600 uppercase">
+                        {label}
+                    </h3>
+                    <div className="flex flex-wrap">
+                        {values.map((value) => {
+                            const { uid, label, swatch_data } = value
+                            const optionData = { attributeCode: attribute_code, valueUid: uid }
+
+                            return (
+                                <div key={uid} className="flex flex-wrap gap-2 p-2" onClick={() => handleOptionSelect(optionData)}>
+                                    {swatch_data ? (
+                                        <div
+                                            className={`w-12 h-12 hover:shadow-[0_0_6px_0_rgb(71,139,255)] rounded 
+                                            ${isSelected(optionData) > -1 ? 'border-2 border-red-500' : ''}`}
+                                        >
+                                            <Swatch {...swatch_data} key={uid} />
+                                        </div>
+                                    ) : (
+                                        <div
+                                            key={uid}
+                                            className={`flex items-center justify-center w-12 h-12 mr-4 mb-4 text-sm text-center text-gray-700 bg-gray-100 border rounded-full cursor-pointer hover:shadow-[0_0_4px_0_rgb(71,139,255)] 
+                                            ${isSelected(optionData) > -1 ? 'border-2 border-red-500' : ''}`}
+                                        >
+                                            {label}
+                                        </div>
+                                    )}
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            )
+        })
+    }, [configurable_options, handleOptionSelect, isSelected])
+
+    useEffect(() => {
+        const matchedVariant = findVariantBySelectedOptions(variants, selectedOptions)
+
+        if (matchedVariant) {
+            setSelectedProductSku(matchedVariant.product.sku)
+        }
+    }, [selectedOptions, variants])
 
     return (
         <div className="flex flex-wrap mb-24 -mx-4">
@@ -82,7 +154,7 @@ export default function ConfigurableProduct({ product, className = '' }: Props) 
                         <ProductPartialPrice price={price_range.maximum_price} />
                         <ProductRating ratingSummary={rating_summary} reviewCount={review_count} />
                     </div>
-                    {renderConfigurableOptions(configurable_options, variants)}
+                    {renderConfigurableOptions()}
                     <div className="flex flex-wrap items-center mb-6">
                         <div className="mb-4 mr-4 lg:mb-0">
                             <div className="w-28">
@@ -115,20 +187,12 @@ export default function ConfigurableProduct({ product, className = '' }: Props) 
                                 </svg>
                             </button>
                         </div>
-                        <a
-                            href="#"
+                        <button
                             className="w-full px-4 py-3 text-center text-blue-600 bg-blue-100 border border-blue-600 hover:bg-blue-600 hover:text-gray-100 lg:w-1/2 rounded-xl"
+                            disabled
                         >
                             Add to cart
-                        </a>
-                    </div>
-                    <div className="flex gap-4 mb-6">
-                        <a
-                            href="#"
-                            className="w-full px-4 py-3 text-center text-gray-100 bg-blue-600 border border-transparent hover:border-blue-500 hover:text-blue-700 hover:bg-blue-100 rounded-xl"
-                        >
-                            Buy now
-                        </a>
+                        </button>
                     </div>
                 </div>
             </div>
